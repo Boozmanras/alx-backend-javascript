a1a523a4fc84716bf48df47d45b9ec852bf8ea17
Boozmanras <paulvictoraa@gmail.com>
2024-10-03 17:31:36 +0300

    changed

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -3,11 +3,9 @@ export class HolbertonClass {
     this._year = year;
     this._location = location;
   }
-
   get year() {
     return this._year;
   }
-
   get location() {
     return this._location;
   }
@@ -19,17 +17,15 @@ export class StudentHolberton {
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
-
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-
   get holbertonClass() {
     return this._holbertonClass;
   }
-
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - \
+${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
 
@@ -42,4 +38,6 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
